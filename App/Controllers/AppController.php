a2d52ffd8de90dfa4b2dd05bf6e48a961e69d022
cp-miv <michael.vogel@ceff.ch>
2026-03-05 12:17:32 +0000

    Fix html filename generation for view

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -9,6 +9,8 @@ use \App\Libs\SessionSecurityHandler;
 use \Core\Controller;
 use \Core\View;
 
+use function Core\Libs\str_lower_camel_case;
+
 abstract class AppController extends Controller
 {
     protected View $view;
@@ -18,7 +20,7 @@ abstract class AppController extends Controller
     protected function before(): bool
     {
         $this->view = new View(templatePath: dirname(__DIR__) . '/Views/');
-        $this->view->setFilename(strtolower($this->routeParams['controller']) . DIRECTORY_SEPARATOR . strtolower($this->routeParams['action']));
+        $this->view->setFilename(str_lower_camel_case($this->routeParams['controller']) . DIRECTORY_SEPARATOR . str_lower_camel_case($this->routeParams['action']));
 
         $this->flash = new FlashNotificationHelper();
 
