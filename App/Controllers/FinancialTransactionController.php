bb5d88386faf4a09cab226927711d31b769fa5ce
cp-miv <michael.vogel@ceff.ch>
2026-03-05 12:45:01 +0000

    Fixe url for case sensitive controllers

--- App/Controllers/FinancialTransactionController.php
+++ App/Controllers/FinancialTransactionController.php
@@ -49,7 +49,7 @@ class FinancialTransactionController extends AppController
         FinancialTransaction::add($financialTransaction);
 
         $this->flash->success('Transaction financière ajoutée');
-        $this->redirect('/financialtransaction/index');
+        $this->redirect('/financialTransaction/index');
     }
 
     public function edit(): void
@@ -85,6 +85,6 @@ class FinancialTransactionController extends AppController
         FinancialTransaction::remove($financialTransaction);
 
         $this->flash->success('Transaction financière supprimée');
-        $this->redirect('/financialtransaction/index');
+        $this->redirect('/financialTransaction/index');
     }
 }
